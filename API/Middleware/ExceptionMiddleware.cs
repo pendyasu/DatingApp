dddbddff0e6ec8e63d8ebb2ca780de6d67e3c9db
Sundeep Pendyala <pendi.chowdary@gmail.com>
2023-09-14 23:53:22 -0400

    End of section 7

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,45 @@
+using System.Net;
+using System.Text.Json;
+using API.Errors;
+using Microsoft.AspNetCore.Http;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private RequestDelegate _next;
+        private ILogger<ExceptionMiddleware> _logger;
+        private IHostEnvironment _env;
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _env = env;
+            _next = next;
+            _logger = logger;
+
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment()
+                    ? new ApiException(context.Response.StatusCode,ex.Message,ex.StackTrace?.ToString())
+                    : new ApiException(context.Response.StatusCode,ex.Message,"Internal Server Error");
+                
+                var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+
+                var json = JsonSerializer.Serialize(response,options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,5 @@
-
-using API.Data;
-using API.Interfaces;
-using Microsoft.EntityFrameworkCore;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +13,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
         .WithOrigins("https://localhost:4200"));
 
